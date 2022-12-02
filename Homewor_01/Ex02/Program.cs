1af212d8c36f8f0f95a2e00b02a44d149d62fb88
udivlennaia <lanakl_76@mail.ru>
2022-12-02 04:20:12 +0300

    Задача 2-Найти большее число из 2х чисел

---
+++ Homewor_01/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//Даны два числа.Покажите большее число.
+
+int a = -8;
+int b = -1;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+Console.WriteLine(max);
+ 
