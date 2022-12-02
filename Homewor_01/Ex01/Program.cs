06db0dcfed104cb2a5395c02624b60f27e2a35f5
udivlennaia <lanakl_76@mail.ru>
2022-12-02 03:34:51 +0300

    Задача 1- Найти квадрат числа

---
+++ Homewor_01/Ex01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("start"); // начало
+
+int number = 11; // определили целое число
+int result = number * number; // вычисление квадрата числа
+Console.WriteLine(result); // вывод результата
+
+Console.WriteLine("end"); // окончание
