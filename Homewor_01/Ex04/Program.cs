6502c230d232a86b1f2391ae66332903dcdc1f5a
udivlennaia <lanakl_76@mail.ru>
2022-12-02 22:23:55 +0300

    Задача 6-Четное число

---
+++ Homewor_01/Ex04/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+// Задача 6.
+// Является ли число четным.
+
+// начало
+
+int num = 155;
+num = even();
+
+if(num / 2  ==1 )
+{
+   Console.WriteLine(even);
+}
+Console.WriteLine("end");
+
+// конец
