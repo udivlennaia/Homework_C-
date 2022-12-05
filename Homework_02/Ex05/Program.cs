6c262527eda5f29cbb726053dcab6f6d9e2a5c58
udivlennaia <lanakl_76@mail.ru>
2022-12-05 09:43:46 +0300

    Сумма двух определенных чисел

---
+++ Homework_02/Ex05/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numberA = 33; // определили первое число
+int numberB = 9; // определили второе число
+Console.WriteLine("Сумма :"); // вывести сумму и перейти на еовую строку
+Console.WriteLine(numberA + numberB); // вывести результат вычисления
