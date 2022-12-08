6c9073c06c4c911161f5db9340bb506233fc3d16
udivlennaia <lanakl_76@mail.ru>
2022-12-08 10:54:53 +0300

    Задача 13. Показать третью цифру числа или сказать, что ее нет.

---
+++ Homework_02/Ex02/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Задача 13. 
+//Напишите программу которая, выводит третью цмфру числа или сообщает 
+// что третьей цифры нет.
+
+
+int a = 7;
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+int b = (int)Math.Floor(Math.Log10(a)) - 2;
+int d = (int)Math.Pow(10,b);
+System.Console.WriteLine(a / d % 10);
