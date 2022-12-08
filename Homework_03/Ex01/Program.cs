55a374a73c80c4be5bbaa47a4a942fb4f7682885
udivlennaia <lanakl_76@mail.ru>
2022-12-08 16:27:20 +0300

    Задача 19.Является ли число палиндромом

---
+++ Homework_03/Ex01/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Задача 19.
+//Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+string inputNumber = Console.ReadLine();
+int number = int.Parse(inputNumber);
+if (10000 < a && a < 100000)
+{
+
+     int [] GreateArray (int count)
+    {
+    return new int[count];
+    }
+    int[]arr = GreateArray(5);
+
+    if (a[0] == a[4] && a[2] == a[3])
+        {
+        Console.WriteLine("Это число палиндром.");
+        }
+
+Console.WriteLine("Это число не является палиндромом.");
+}
