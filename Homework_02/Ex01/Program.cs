fa625bc5d43afc49dfdf6f7036286ccf22cab4e8
udivlennaia <lanakl_76@mail.ru>
2022-12-08 09:46:05 +0300

    Задача 10. Показать вторую цифру трехзначного числа.

---
+++ Homework_02/Ex01/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Задача 10.
+//Напишите программу,которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int a = 589 ;
+System.Console.WriteLine((a / 10) % 10);
