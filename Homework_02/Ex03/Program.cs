8106085a472d6c7e5e7b74fcf8b8851d42f07ce6
udivlennaia <lanakl_76@mail.ru>
2022-12-08 13:01:53 +0300

    Задача 15.Вывести выходной/рабочий день недели

---
+++ Homework_02/Ex03/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// Задача 15.
+// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели и проверяет, яявляется ли этот день выходным.
+
+Console.WriteLine("Введите день недели в виде числа: ");
+int a = int.Parse(Console.ReadLine());
+if (0 < a && a <= 5)
+{
+    Console.WriteLine("Это рабочий день.");
+}
+if ( 5 < a  && a <= 7 )
+{
+    Console.WriteLine("Это выходной день !");
+}
