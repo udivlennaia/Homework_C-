f819cd8a77dd5a7c326118511d8536e254a2d2d9
udivlennaia <lanakl_76@mail.ru>
2022-12-14 23:20:07 +0300

    Задача 23. Таблица кубов.

---
+++ Homework_03/Ex02/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//Задача 23
+
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+
+for( int i = 2; i <= 10; i++)
+{
+ System.Console.WriteLine($"{i,3} x {i,3} x {i,3}= {i * i * i}");
+}
+ System.Console.WriteLine();
+
+
+
+
+
+
